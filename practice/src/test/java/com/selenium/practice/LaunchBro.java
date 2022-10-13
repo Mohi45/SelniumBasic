0a1c9f047e396985d29eea95fdc89c4ea9783211
Mohit Kumar <mk908990@gmail.com>
2022-10-13 12:02:03 +0530

    add logs

--- practice/src/test/java/com/selenium/practice/LaunchBro.java
+++ practice/src/test/java/com/selenium/practice/LaunchBro.java
@@ -8,14 +8,18 @@ public class LaunchBro {
     @Test
     public static void main()
     {
+        System.out.println("1st Line inside Method !!!");
         System.setProperty("webdriver.chrome.driver", "/usr/bin/google-chrome");
+        System.out.println("2nd Line inside Method !!!");
         // Instantiate a ChromeDriver class.
         WebDriver driver = new ChromeDriver();
+        System.out.println("3rd Line inside Method !!!");
 
         // Maximize the browser
         driver.manage().window().maximize();
-
+        System.out.println("4th Line inside Method !!!");
         // Launch Website
         driver.get("https://www.geeksforgeeks.org/");
+        System.out.println("5th Line inside Method !!!");
     }
 }
