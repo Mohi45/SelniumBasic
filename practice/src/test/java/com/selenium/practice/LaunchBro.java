d10400bd67c6f7c248dfa49a7b97e41cc94f3312
nks4087 <61387632+nks4087@users.noreply.github.com>
2022-10-13 15:39:48 +0530

    Update LaunchBro.java

--- practice/src/test/java/com/selenium/practice/LaunchBro.java
+++ practice/src/test/java/com/selenium/practice/LaunchBro.java
@@ -9,15 +9,10 @@ public class LaunchBro {
     public static void main()
     {
         System.out.println("1st Line inside Method !!!");
-         System.out.println("1st Line inside Method !!!");
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         System.out.println("2nd Line inside Method !!!");
         // Instantiate a ChromeDriver class.
         WebDriver driver = new ChromeDriver();
-        chrome_options = driver.ChromeOptions();
-        chrome_options.add_argument("--no-sandbox");
-        chrome_options.add_argument("--disable-dev-shm-usage");
-        chrome_options.add_argument('--headless');
             
         System.out.println("3rd Line inside Method !!!");
 
